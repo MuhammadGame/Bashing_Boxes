7a940902c0c83acaa70d36a2a576d9db08e64fde
MuhammadGameVM <MuhAbdulla@smail.pembinatrails.ca>
2026-01-21 15:28:15 -0600

    2nd push

--- Game.sh
+++ Game.sh
@@ -1,11 +1,32 @@
 #!bin/bash
 
+	y/n_choices(){
+
+		while true; do
+		read -p "$name want to play a game?" answer
+
+			case $answer in
+			Y|y|yes|Yes) echo " Lets play rock,paper,scissors"
+			;;
+			n|N|no|No) echo " its okay, you can LEAVE!!"
+			exit
+			;;
+			*) echo e"
+
+				 y/n"
+			continue
+			;;
+			
+			esac
+		done 
+
+	}
+
+
 	read -p "hi, my name is MOBRO whats yours" name
+	echo -e "
 
-	echo "hey $name want to play a game"
-	read answer
+	"
 
-	case $answer in
-	Y|y|yes|Yes) echo " Lets play rock,paper,scissors"
-	;;
-	n|N|no|No) echo " its okay, you can LEAVE!!"
+	y/n_choices
+
