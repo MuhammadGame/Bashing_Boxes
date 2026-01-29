1f0c3674cd526fda295d0e1b4108defec89cf27e
MuhammadGameVM <MuhAbdulla@smail.pembinatrails.ca>
2026-01-29 14:51:28 -0600

    Added Game.sh

--- Game.sh
+++ Game.sh
@@ -14,12 +14,16 @@ Player_chooses(){
 		case $answer2 in
 		A|a|Rock)
 			User_choice="Rock"
-			;;
+			Game
+		;;
 		B|b|Paper)
 			User_choice="Paper"
+			Game
 		;;
 		C|c|Scissors)
 			User_choice="Scissors"
+			Game
+
 		;;
 		*) echo "try again"
 		y/n_choices 
@@ -41,7 +45,6 @@ echo  "${Comp_choices[$RANDOM % ${#Comp_choices[@]}]}"
 	Game(){
 
 	Comp_choice=$(Clanker_chooses)
-	Player_chooses
 
 	echo "You chose $User_choice"
 	echo "PC chose $Comp_choice"
@@ -58,8 +61,23 @@ echo  "${Comp_choices[$RANDOM % ${#Comp_choices[@]}]}"
 
  
 
-	fi	
-
+	fi
+	for (( j=1; j<=1; )); do
+		read -p "$name want to play again? " answer3
+		case $answer3 in
+		y|yes|Yes|Y) echo -e "/n/n"
+		Player_chooses
+		(( j=2 ))
+		;;
+		No|n|N|no) echo "thanks for playing tho"
+		exit
+		(( J=2 ))
+		;;
+		*) echo -e "/nyes or no"
+		continue
+		;;
+		esac
+	done
 
 	}
 
@@ -73,7 +91,7 @@ y/n_choices(){
 		Y|y|yes|Yes) echo " Lets play rock,paper,scissors" 
 		(( i=2 ))
 		echo "$i"
-		Game
+		Player_chooses
 		;;
 		n|N|no|No) echo " its okay, you can LEAVE!!"
 		(( i=2 ))
@@ -89,7 +107,7 @@ y/n_choices(){
 }
 
 
-	read -p "hi, my name is MOBRO whats yours" name
+	read -p "hi, my name is MOBRO whats yours " name
 	echo -e "
 
 	"
