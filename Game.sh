c1ebf0adb1e3b8eba6fabc2a5cf486c0b1cae7c5
MuhammadGameVM <MuhAbdulla@smail.pembinatrails.ca>
2026-01-29 12:27:39 -0600

    Added Game.sh

--- Game.sh
+++ Game.sh
@@ -1,4 +1,44 @@
 #!bin/bash
+Clanker_game(){
+
+	local Clanker_choice=("Rock" "Paper" "Scissors" )
+
+echo  "${Clanker_choice[$RANDOM % ${#Clanker_choice[@]}]}"
+
+}
+
+
+		Player_game(){
+		echo -e "
+
+
+
+		"
+	while true ; do
+			read -p "try again"
+
+
+		read   -p " 
+		A) Rock
+		B) Paper
+		C) Scissors " answer2
+
+		case $answer2 in
+		A|a|Rock)
+			;;
+		B|b|Paper)
+		;;
+		C|c|Scissors)
+		;;
+		*) echo "try again"
+		continue
+		;;
+
+		esac
+	done
+
+		}
+
 
 	y/n_choices(){
 
@@ -6,7 +46,8 @@
 			read -p "$name want to play a game?" answer
 
 			case $answer in
-			Y|y|yes|Yes) echo " Lets play rock,paper,scissors"
+			Y|y|yes|Yes) echo " Lets play rock,paper,scissors" 
+			Player_game
 			;;
 			n|N|no|No) echo " its okay, you can LEAVE!!"
 			exit
